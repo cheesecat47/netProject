f6d40e5a39af183b0267df8849aa64e05390669b
cheesecat47 <cheesecat47@gmail.com>
2019-05-23 16:37:07 +0900

    server 수정

--- server.py
+++ server.py
@@ -11,32 +11,35 @@ def out(msg):
 
 port = 55891
 flag = True
+cnt = 0
 
 while flag:
-    fname = './log/' + str(dt.now())[:10] + '-' + str(dt.now().hour) + '-' + str(dt.now().minute) \
-            + '-' + str(dt.now().second) + '.txt'
-    log = open(fname, 'w')
-
     serverSock = socket(AF_INET, SOCK_STREAM)
     serverSock.bind(('', port))
     serverSock.listen(1)
 
-    out(str(dt.now()) + ': %d 포트 대기...' % port)
+    print(str(dt.now()) + ': %d 포트 대기...' % port)
 
     connectionSock, addr = serverSock.accept()
 
+    fname = './log/' + str(dt.now())[:10] + '-' + str(dt.now().hour) + '-' + str(dt.now().minute) \
+            + '-' + str(dt.now().second) + '.txt'
+    log = open(fname, 'w')
+
     out(str(dt.now()) + ' ' + str(addr) + ' ' + ': 에서 접속되었습니다.')
+
+    cnt += 1
+    print(str(cnt) + "번째 통신")
 
     while True:
         recvData = connectionSock.recv(1024).decode('utf-8')
         out(str(dt.now()) + ' ' + str(addr) + ' ' + ': ' + recvData)
 
         if recvData == 'exit':
-            flag = False
             break
 
         sendData = 'server: echo: ' + recvData
         connectionSock.send(sendData.encode('utf-8'))
 
     log.close()
-    #end while flag
+    # end while flag
