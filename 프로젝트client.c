f3978170ec3abe6658652c1e9745e6f3072edeeb
atg0831 <44899448+atg0831@users.noreply.github.com>
2019-06-12 01:53:29 +0900

    Update 프로젝트client.c

--- 프로젝트client.c
+++ 프로젝트client.c
@@ -10,8 +10,8 @@
 #define BUF_SIZE 1024
 void error_handling(char *message);
 char* drawHangman(int num);
-void print_turn(char *player[], char *turn, int i);
-void print_player(char *player[], int i);
+void print_turn(char *player[], char *turn);
+//void print_player(char *player[], int i);
 
 int main(int argc, char *argv[])
 {
@@ -37,51 +37,19 @@ int main(int argc, char *argv[])
 
 	memset(&servAddr, 0, sizeof(servAddr));
 	servAddr.sin_family = AF_INET;
-	//servAddr.sin_addr.s_addr = inet_addr("155.230.28.129");
-	servAddr.sin_addr.s_addr = inet_addr("118.45.154.188");
+	servAddr.sin_addr.s_addr = inet_addr("155.230.28.129");
+	//servAddr.sin_addr.s_addr = inet_addr("118.45.154.188");
 
 	servAddr.sin_port = htons(atoi("45454"));
 
 	if (connect(hSocket, (struct sockaddr*)&servAddr, sizeof(servAddr)) == -1)
 		error_handling("connect() error!");
 	else
-		puts("Connected...........\n");
-
-
-	//printf("player 이름을 입력해주세요 :");
-	//fgets(user_name, sizeof(user_name), stdin);
-	//temp = strtok_s(user_name, "\n", &k);
-
-/*	printf("게임이 준비된 클라이언트는 'r'을 입력, 불참을 원하시면 'e'를 입력해주세요\n\n");
-	ch = getchar();
-
-	while (1)
-	{
-		getchar();
-		if (ch == 'r')
-		{
-			//str_len = send(hSocket, "ready", strlen("ready"), 0);
-			str_len = send(hSocket, user_name, strlen(user_name), 0);
-			//getchar();
-			break;
-		}
-		else if (ch == 'e')
-		{
-			str_len = send(hSocket, "exit", strlen("exit"), 0);
-			printf("종료하겠습니다.\n");
-			closesocket(hSocket);
-			WSACleanup();
-			return 0;
-		}
-		else
-		{
-			printf("'r'또는 'e'만 입력해주세요\n");
-			ch = getchar();
-		}
-	}
-
-printf("다른 player들의 입장을 기다립니다........\n");              
+		puts("Connected...........\n게임에 입장합니다. \n");
+	/*
+	printf("다른 player들의 입장을 기다립니다........\n");              
 	*/
+	
 	memset(token_player, '\0', sizeof(token_player));
 
 	recv_len = recv(hSocket, token_player, BUF_SIZE, 0);
@@ -89,70 +57,25 @@ printf("다른 player들의 입장을 기다립니다........\n");
 	if (recv_len == -1 || recv_len == 0)
 	{
 		printf("서버로부터 player 리스트 받기 실패\n");
-		//exit(1);
+		exit(1);
 	}
-//	printf("dfsadsfs");
+
 	printf("당신은 player %s입니다.\n", token_player);
 
-
-
-	/*
-	클라이언트 플레이어 수 반복으로 돌리자
-
-
-	*/
-
-	/*
-	players[0] = strtok_s(token_player, "/", &players[1]);
-	players[1] = strtok_s(players[1], "/", &players[2]);
-	players[2] = strtok_s(players[2], "/", &players[3]);
-	print_player(players, 3);
-	*/
-	
-
-/*	int i=0;
-players[0] = strtok(token_player, "/");// , &players[1]);
-	while (1)
-	{
-		i++;
-		players[i] = strtok(NULL, "/");//, &players[i+1]);
-		
-		if(players[i] == NULL)
-			break;
-		//players[2] = strtok_s(players[2], "/", &players[3]);
-		//
-	}
-	print_player(players, i - 1);
-	
-
-
-	/*
-	참가 user 출력
-	recv
-	//'/'로 parsing
-	//player[]에 저장
-//	print
-	*/
-	//	ch = getchar();
-	
 	recv_len = 0;
 
 	while (1)
 	{
 		recv_len = 0;
 		memset(user_info, '\0', sizeof(message));
-
-		//str_len = 0;
-		//while (recv_len < str_len)
-		//{
-			recv_cnt = recv(hSocket, user_info, BUF_SIZE, 0);
-			if (recv_cnt == -1)
-				error_handling("read() error!");
-			else if (recv_cnt == 0)printf("No data received\n");
-			recv_len += recv_cnt;
-		//}
-
-		printf("일단 확인하기 위해서%s\n", user_info);
+		recv_cnt = recv(hSocket, user_info, BUF_SIZE, 0);
+		if (recv_cnt == -1)
+			error_handling("read() error!");
+		else if (recv_cnt == 0)
+			printf("No data received\n");
+		recv_len += recv_cnt;
+		
+		//printf("일단 확인하기 위해서%s\n", user_info);
 		turn = strtok_s(user_info, "/", &chanceStr);
 		chanceStr = strtok_s(chanceStr, "/", &word_now);
 		word_now = strtok_s(word_now, "/", &tf);
@@ -162,70 +85,32 @@ players[0] = strtok(token_player, "/");// , &players[1]);
 
 
 		if (strcmp(tf, token_player) == 0)
-		{
-			printf("정답 입니다.\n");
-			//chanceStr = "7";
-		}
-
-		//else if (strcmp(tf, "-1") != 0)
+			printf("\n정답 입니다.\n");
 		else if(strcmp(tf,token_player)!=0 &&strcmp(tf,token_player)!=-1)
-			printf("%s가 정답을 맞추었습니다.", tf);
+			printf("\n%s가 정답을 맞추었습니다.", tf);
 
-
-
-		print_turn(token_player, turn, 3);
+		print_turn(token_player, turn);
 
 		if (strcmp(turn, token_player) == 0)
 		{
-			printf("게임을 시작하겠습니다. 종료하시고 싶으시면 'exit'를 입력해주세요\n");
+			printf("게임을 시작하겠습니다. \n");
 			puts("단어 혹은 알파벳을 입력하세요 : ");
 			fgets(message, BUF_SIZE, stdin);
 
 			//if (strcmp(message, "\n") == 0)
 				//continue;
-
-			if (!strcmp(message, "exit\n") || (!strcmp(message, "EXIT\n")))
-			{
-				//str_len = send(hSocket, message, strlen(message), 0);
-				break;
-			}
-
 			str_len = send(hSocket, message, strlen(message), 0);
 		}
 
 
-		//printf("%s", tf);
-		//print_turn(players, turn, 3);
-
-	/*	if (strcmp(turn,players[3]) == 0)	//본인이름과 server가 보낸 turn 비교해서 같으면 진행
-		{
-			printf("게임을 시작하겠습니다. 종료하시고 싶으시면 'exit'를 입력해주세요\n");
-			puts("단어 혹은 알파벳을 입력하세요 : ");
-			fgets(message, BUF_SIZE, stdin);
-
-			if (strcmp(message, "\n") == 0)
-				continue;
-
-			if (!strcmp(message, "exit\n") || (!strcmp(message, "EXIT\n")))
-			{
-				//str_len = send(hSocket, message, strlen(message), 0);
-				break;
-			}
-
-			str_len = send(hSocket, message, strlen(message), 0);
-		}
-		else
-			printf("당신의 turn이 아닙니다. turn이 돌아올때까지 대기해주세요\n");
-
-
-	}
-	*/
+	
 	}
 	closesocket(hSocket);
 	WSACleanup();
 	return 0;
 }
 
+/*
 void print_player(char *player[], int i)
 {
 
@@ -238,28 +123,18 @@ void print_player(char *player[], int i)
 	}
 	printf("-----------------------------------\n");
 }
-
-void print_turn(char *player, char *turn, int i)
+*/
+void print_turn(char *player, char *turn)
 {
-	//int i;
-	int turn_ind = atoi(turn);
 
+	if (strcmp(turn, "-1") == 0)
+		printf("게임이 종료되었습니다.\n");
 	printf("\n-----------------------------------\n");
-
 	if (strcmp(player, turn) == 0)
 		printf("나의 차례입니다.\n");
 	else
 		printf("player %s의 차례입니다. 대기하세요\n",turn);
 	
-	/*for (i = 0; i < 3; i++)
-	{
-		//strcat(player[i], "\n");
-		//if (strcmp(turn, player[i]) == 0)
-		if(turn_ind==i)
-		printf("%s<-\n", player[i]);
-		else
-			printf("%s\n", player[i]);
-	}*/
 	printf("-----------------------------------\n");
 
 }
